e1021e248b14498ee263fd78f05f93a1039e9240
Anant <anantcode@gmail.com>
2021-05-23 21:03:33 +0530

    Adds react router basic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header/Header";
 import LoginForm from "./components/LoginForm/LoginForm";
+import Inbox from "./components/Inbox/Inbox";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
     return (
-        <div>
+        <main>
+            {/* <Navbar /> */}
             <Header />
-            <LoginForm />
-        </div>
+            <Switch>
+                <Route path="/" component={LoginForm} exact />
+                {/* <Route path="/about" component={About} /> */}
+                <Route path="/login" component={LoginForm} />
+                <Route path="/inbox" component={Inbox} />
+                <Route component={Error} />
+            </Switch>
+        </main>
     );
 }
 
--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 const appStyle = {
     height: "250px",
@@ -78,11 +79,15 @@ const Form = ({ onSubmit }) => {
 // Usage example:
 
 const LoginForm = () => {
+    const history = useHistory();
+
     const handleSubmit = (data) => {
         const json = JSON.stringify(data, null, 4);
         console.clear();
         console.log(json);
+        history.push("/inbox");
     };
+
     return (
         <div style={appStyle}>
             <Form onSubmit={handleSubmit} />
