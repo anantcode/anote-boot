b8794475d013f880bb329e4df507af7de2a795cf
Anant <anantcode@gmail.com>
2021-05-24 22:07:38 +0530

    Added scroll component

--- src/components/Inbox/Inbox.js
+++ src/components/Inbox/Inbox.js
@@ -1,9 +1,35 @@
 import React from "react";
+import Scroll from "../../components/Scroll/Scroll";
 
 const Inbox = () => {
     return (
         <div>
-            <h1>Test Inbox</h1>
+            <Scroll>
+                <h1>Test Inbox</h1>
+                <h1>Test Inbox</h1>
+                <h1>Test Inbox</h1>
+                <h1>Test Inbox</h1>
+                <h1>Test Inbox</h1>
+                <h1>Test Inbox</h1>
+
+                <h1>Test Inbox</h1>
+                <h1>Test Inbox</h1>
+                <h1>Test Inbox</h1>
+                <h1>Test Inbox</h1>
+                <h1>Test Inbox</h1>
+
+                <h1>Test Inbox</h1>
+                <h1>Test Inbox</h1>
+                <h1>Test Inbox</h1>
+                <h1>Test Inbox</h1>
+                <h1>Test Inbox</h1>
+
+                <h1>Test Inbox</h1>
+                <h1>Test Inbox</h1>
+                <h1>Test Inbox</h1>
+                <h1>Test Inbox</h1>
+                <h1>Test Inbox</h1>
+            </Scroll>
         </div>
     );
 };
